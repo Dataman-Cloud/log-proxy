6d8aa36b4061e290d2c5d6f0b5db113e84858099
my9074 <313231068@qq.com>
2016-11-22 17:03:17 +0800

    Update log html

--- frontend/src/log/log.ctrl.js
+++ frontend/src/log/log.ctrl.js
@@ -3,13 +3,17 @@
     angular.module('app')
         .controller('LogCtrl', LogCtrl);
     /* @ngInject */
-    function LogCtrl() {
+    function LogCtrl(logBackend, moment) {
         var self = this;
+        self.curTimestamp = moment().unix() * 1000;
+        self.fromTimestamp = moment().subtract(1, 'hours').unix() * 1000;
 
         activate();
 
         function activate() {
-
+            logBackend.listApp({from: self.fromTimestamp, to: self.curTimestamp}).get(function (data) {
+                console.log(data)
+            })
         }
     }
 })();
---
+++ frontend/src/log/service/log-backend.service.js
@@ -0,0 +1,55 @@
+(function () {
+    'use strict';
+    angular.module('app')
+        .factory('logBackend', logBackend);
+    /* @ngInject */
+    function logBackend($resource) {
+        return {
+            listApp: listApp,
+            listTask: listTask,
+            listPath: listPath,
+            searchLogs: searchLogs
+        };
+
+        function listApp(data) {
+            var paramObj = data || {};
+            return $resource(BACKEND_URL_BASE.defaultBase + '/v1/search/applications', {
+                from: paramObj.from,
+                to: paramObj.to
+            });
+        }
+
+        function listTask(data) {
+            var paramObj = data || {};
+            return $resource(BACKEND_URL_BASE.defaultBase + '/v1/search/tasks/:appid', {
+                appid: paramObj.appid,
+                from: paramObj.from,
+                to: paramObj.to
+            });
+        }
+
+        function listPath(data) {
+            var paramObj = data || {};
+            return $resource(BACKEND_URL_BASE.defaultBase + '/v1/search/paths/:appid/:taskid', {
+                appid: paramObj.appid,
+                taskid: paramObj.taskid,
+                from: paramObj.from,
+                to: paramObj.to
+            });
+        }
+
+        function searchLogs(data) {
+            var paramObj = data || {};
+            return $resource(BACKEND_URL_BASE.defaultBase + '/v1/search/index', {
+                appid: paramObj.appid,
+                taskid: paramObj.taskid,
+                path: paramObj.path,
+                keyword: paramObj.keyword,
+                from: paramObj.from,
+                to: paramObj.to,
+                page: paramObj.page,
+                size: paramObj.size
+            });
+        }
+    }
+})();
