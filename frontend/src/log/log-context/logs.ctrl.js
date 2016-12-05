53f027e4f991d0f517903d33eae959a90f216f25
my9074 <313231068@qq.com>
2016-12-05 15:40:22 +0800

    Add md-top-index for md-virtual-repeat-container

--- frontend/src/log/log-context/logs.ctrl.js
+++ frontend/src/log/log-context/logs.ctrl.js
@@ -3,7 +3,7 @@
     angular.module('app')
         .controller('LogContextCtrl', LogContextCtrl);
     /* @ngInject */
-    function LogContextCtrl(logBackend, $stateParams) {
+    function LogContextCtrl(logBackend, $stateParams, $timeout) {
         var self = this;
 
         self.contextQueryObj = {
@@ -15,6 +15,7 @@
             size: 100
         };
         self.logContexts = [];
+        self.offsetIndex = 0;
 
         //Infinite Scroll setting
         self.infiniteLogs = {
@@ -45,6 +46,7 @@
                         } else {
                             self.contextQueryObj.page += 1;
                             this.items = this.items.concat(data.data);
+                            $timeout(checkContextIndex, 0);
                             this.numLoaded_ = this.toLoad_;
                         }
                     }));
@@ -55,6 +57,15 @@
         activate();
 
         function activate() {
+        }
+
+        function checkContextIndex() {
+            angular.forEach(self.infiniteLogs.items, function (log, index) {
+                if (log.offset == $stateParams.offset) {
+                    self.offsetIndex = index;
+                }
+            });
+
         }
     }
 })();
