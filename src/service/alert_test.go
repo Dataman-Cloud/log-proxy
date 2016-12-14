67e7b3b7fc8e85f28db8631fbc724965f1afdafe
yqguodataman <yqguo@dataman-inc.com>
2016-12-14 11:32:13 +0800

    update

---
+++ src/service/alert_test.go
@@ -0,0 +1,58 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/Dataman-Cloud/log-proxy/src/config"
+	"github.com/Dataman-Cloud/log-proxy/src/models"
+)
+
+func TestCreateAlert(t *testing.T) {
+	config.InitConfig("../../env_file.template")
+	service := NewEsService([]string{baseUrl})
+	if service.CreateAlert(new(models.Alert)) == nil {
+		t.Log("success")
+	} else {
+		t.Error("faild")
+	}
+}
+
+func TestGetAlerts(t *testing.T) {
+	config.InitConfig("../../env_file.template")
+	service := NewEsService([]string{baseUrl})
+	if _, err := service.GetAlerts(models.Page{}); err == nil {
+		t.Log("success")
+	} else {
+		t.Error("faild")
+	}
+}
+
+func TestGetAlert(t *testing.T) {
+	config.InitConfig("../../env_file.template")
+	service := NewEsService([]string{baseUrl})
+	if _, err := service.GetAlert("test"); err == nil {
+		t.Log("success")
+	} else {
+		t.Error("faild")
+	}
+}
+
+func TestDeleteAlert(t *testing.T) {
+	config.InitConfig("../../env_file.template")
+	service := NewEsService([]string{baseUrl})
+	if service.DeleteAlert("test") == nil {
+		t.Log("success")
+	} else {
+		t.Error("faild")
+	}
+}
+
+func TestUpdateAlert(t *testing.T) {
+	config.InitConfig("../../env_file.template")
+	service := NewEsService([]string{baseUrl})
+	if service.UpdateAlert(new(models.Alert)) == nil {
+		t.Log("success")
+	} else {
+		t.Error("faild")
+	}
+}
