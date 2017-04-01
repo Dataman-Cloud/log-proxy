e6cadedfa605bce6b515cd9ed5fe2244de9b2968
Zhongqiang Dou <douzl0814@163.com>
2017-04-01 08:18:54 +0800

    Add the rule indicater tomcat_threadpool_currentthreadcount
    Signed-off-by: Zhongqiang Dou <douzl0814@163.com>

--- src/utils/prometheusrule/indicator_template.go
+++ src/utils/prometheusrule/indicator_template.go
@@ -8,7 +8,7 @@ var filter = "id=~\"/docker/.*\", name=~\"mesos.*\", container_label_VCLUSTER=\"
 
 var cpuUsagePercent = &models.Indicator{
 	Name:  "cpu_usage",
-	Templ: "%s(rate(container_cpu_usage_seconds_total{" + filter + "}[%s])) by (container_env_mesos_task_id) keep_common * 100 %s %s",
+	Templ: "%s(irate(container_cpu_usage_seconds_total{" + filter + "}[%s])) by (container_env_mesos_task_id) keep_common * 100 %s %s",
 	Unit:  "%",
 }
 
@@ -19,9 +19,16 @@ var memUsagePercent = &models.Indicator{
 	Unit: "%",
 }
 
+var tomcatThreadPool = &models.Indicator{
+	Name:  "tomcat_thread_count",
+	Templ: "%s(tomcat_threadpool_currentthreadcount{" + filter + "}) by (container_env_mesos_task_id) keep_common %s %s",
+	Unit:  "个",
+}
+
 func GetRuleIndicators() map[string]*models.Indicator {
 	var ruleIndicators = make(map[string]*models.Indicator)
 	ruleIndicators[cpuUsagePercent.Name] = cpuUsagePercent
 	ruleIndicators[memUsagePercent.Name] = memUsagePercent
+	ruleIndicators[tomcatThreadPool.Name] = tomcatThreadPool
 	return ruleIndicators
 }
