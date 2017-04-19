820d604c7543cda8d0f7a255fb53a161b6432833
Zhongqiang Dou <douzl0814@163.com>
2017-04-19 14:51:48 +0800

    Fix the bug of alert rule
    Signed-off-by: Zhongqiang Dou <douzl0814@163.com>

--- src/utils/prometheusrule/indicator_template.go
+++ src/utils/prometheusrule/indicator_template.go
@@ -8,20 +8,20 @@ var filter = "id=~\"/docker/.*\", name=~\"mesos.*\", container_label_VCLUSTER=\"
 
 var cpuUsagePercent = &models.Indicator{
 	Name:  "cpu_usage",
-	Templ: "%s(irate(container_cpu_usage_seconds_total{" + filter + "}[%s])) by (container_env_mesos_task_id) keep_common * 100 %s %s",
+	Templ: "%s(irate(container_cpu_usage_seconds_total{" + filter + "}[%s])) by (container_label_APP_ID) keep_common * 100 %s %s",
 	Unit:  "%",
 }
 
 var memUsagePercent = &models.Indicator{
 	Name: "mem_usage",
 	Templ: "%s(container_memory_usage_bytes{" + filter +
-		"} / container_spec_memory_limit_bytes{" + filter + "}) by (container_env_mesos_task_id) keep_common * 100 %s %s",
+		"} / container_spec_memory_limit_bytes{" + filter + "}) by (container_label_APP_ID) keep_common * 100 %s %s",
 	Unit: "%",
 }
 
 var tomcatThreadPool = &models.Indicator{
 	Name:  "tomcat_thread_count",
-	Templ: "%s(tomcat_threadpool_currentthreadcount{" + filter + "}) by (container_env_mesos_task_id) keep_common %s %s",
+	Templ: "%s(tomcat_threadpool_currentthreadcount{" + filter + "}) by (container_label_APP_ID) keep_common %s %s",
 	Unit:  "个",
 }
 
